7a6f079739f987fb44812ba57669f41cb7ab6229
nadezhdagub <41242817+nadezhdagub@users.noreply.github.com>
2022-05-07 21:04:36 +0300

    Update Controller.java

--- 2048/src/Controller.java
+++ 2048/src/Controller.java
@@ -23,21 +23,27 @@ public class Controller extends KeyAdapter {
                 case KeyEvent.VK_LEFT:
                     model.left();
                     break;
+                    
                 case KeyEvent.VK_RIGHT:
                     model.right();
                     break;
+                    
                 case KeyEvent.VK_UP:
                     model.up();
                     break;
+                    
                 case KeyEvent.VK_DOWN:
                     model.down();
                     break;
+                    
                 case KeyEvent.VK_Z:
                     model.rollback();
                     break;
+                    
                 case KeyEvent.VK_R:
                     model.randomMove();
                     break;
+                    
                 case KeyEvent.VK_A:
                     model.autoMove();
                     break;
